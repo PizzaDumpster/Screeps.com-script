da367b531191c59a4295b17201f938fa7755f05f
PizzaDumpster <djd187@hotmail.com>
2024-03-21 10:19:22 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { filter, kebabCase } = require("lodash");
+const { filter } = require("lodash");
 
 // BODYPART_COST: { "move": 50, "work": 100, "attack": 80, "carry": 50, "heal": 250, "ranged_attack": 150, "tough": 10, "claim": 600 }
 var PHASE = 0;
